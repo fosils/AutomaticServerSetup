006e03a5facce8e68767b3d527e74dd6ffad35bf
Krzysztof Raczyński <raczman@gmail.com>
2012-04-02 23:16:36 +0200

    Bug fix in setup_python.py now pip will be installed properly

--- tasks/setup_python.py
+++ tasks/setup_python.py
@@ -3,5 +3,5 @@ from fabric import operations as op
 
 def run(aws):
     op.run('sudo yum install python python-devel gcc -y')
-    op.run('sudo easy_install install pip')
+    op.run('sudo easy_install pip')
     op.run('sudo pip install boto fabric')
