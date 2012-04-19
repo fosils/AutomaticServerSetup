4fa13fb06c8a42ec135fd2a99681cdecee6c7025
Chris Moore <dooglus@gmail.com>
2012-04-19 14:26:59 -0700

    New file for setting up useful bash aliases.

---
+++ tasks/bashrc.py
@@ -0,0 +1,22 @@
+from fabric import operations as op
+
+def run(aws):
+
+    bashrc_content = '''
+alias l='ls -l'
+alias la='ls -al'
+alias lt='ls -altr'
+alias mv='mv -i'
+alias cp='cp -i'
+alias rm='rm -i'
+alias j='jobs'
+alias xa='tr \\\\\\\\\\\\\\\\012 \\\\\\\\\\\\\\\\000 | xargs -0'
+alias sql-root='mysql -uroot -p%s mysql'
+alias sql-lamp='mysql -u%s -p%s LAMPCMS'
+alias sql-titles='echo "select qid, title from question_title" | sql-lamp'
+alias sa='. ~/.bashrc'
+''' % (aws.options.mysql_root_pw,
+       aws.options.mysql_lamp_user, aws.options.mysql_lamp_pw)
+
+    op.run('echo Setting up ~/.bashrc ...')
+    op.run("cat >> .bashrc << EOF%sEOF" % bashrc_content)
