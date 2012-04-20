2a316bbf9a6f876ea048b7a3e1561b958c810ca4
Chris Moore <dooglus@gmail.com>
2012-04-19 18:10:01 -0700

    Add 'sync-mysql' alias to sync questions from MongoDB to MySQL.

--- tasks/bashrc.py
+++ tasks/bashrc.py
@@ -15,7 +15,9 @@ alias sql-root='mysql -uroot -p%s mysql'
 alias sql-lamp='mysql -u%s -p%s LAMPCMS'
 alias sql-titles='echo "select qid, title from question_title" | sql-lamp'
 alias sa='. ~/.bashrc'
+alias sync-mysql='echo "delete from question_title" | sql-lamp; php /var/www/lib/Lampcms/Modules/Search/SyncFromMongoDB.php %s %s'
 ''' % (aws.options.mysql_root_pw,
+       aws.options.mysql_lamp_user, aws.options.mysql_lamp_pw,
        aws.options.mysql_lamp_user, aws.options.mysql_lamp_pw)
 
     op.run('echo Setting up ~/.bashrc ...')
