9efc53206707f05e8de38b33f3614bcf56319c8b
Chris Moore <dooglus@gmail.com>
2012-04-19 23:16:38 -0700

    Set an environment variable $DOMAIN in ec2-user's shell.

--- tasks/bashrc.py
+++ tasks/bashrc.py
@@ -3,6 +3,7 @@ from fabric import operations as op
 def run(aws):
 
     bashrc_content = '''
+export DOMAIN='%s'
 alias l='ls -l'
 alias la='ls -al'
 alias lt='ls -altr'
@@ -16,7 +17,7 @@ alias sql-lamp='mysql -u%s -p%s LAMPCMS'
 alias sql-titles='echo "select qid, title from question_title" | sql-lamp'
 alias sa='. ~/.bashrc'
 alias sync-mysql='echo "delete from question_title" | sql-lamp; php /var/www/lib/Lampcms/Modules/Search/SyncFromMongoDB.php %s %s'
-''' % (aws.options.mysql_root_pw,
+''' % (aws.hostname, aws.options.mysql_root_pw,
        aws.options.mysql_lamp_user, aws.options.mysql_lamp_pw,
        aws.options.mysql_lamp_user, aws.options.mysql_lamp_pw)
 
