3facdd9217ca70f03489d29ed0af766104a8722f
David Andersen <mrdavidandersen@gmail.com>
2012-05-13 13:45:01 +0000

    Now sets the email address of the Q&A site to the one specified in the web-form

--- tasks/qanda.py
+++ tasks/qanda.py
@@ -2,6 +2,9 @@ from __future__ import with_statement
 from fabric import context_managers as cm
 from fabric import operations as op
 
+def replace_in_file(file, before, after):
+    op.run("sudo sed --in-place 's|%s|%s|' '%s'" % (before, after, file))
+
 # command history to setup QandA site on Amazon Linux AMI
 def run(aws):
     aws.run_task('update_packages')
@@ -23,4 +26,5 @@ def run(aws):
         op.run('echo Installing QA-website')
         op.run('sudo mv -f fosils-QA-*/* ./')
         op.run('sudo rmdir fosils-QA-*')
-        op.run('service httpd restart')
+        replace_in_file('/var/www/html/modules/question.php', 'mrdavidandersen@gmail.com', aws.options.admin_email)
+        op.run('sudo service httpd restart')
